de6bc64ce206f47dcecac9611f5f08760551e4ae
huibudc <xiang1990_ok@126.com>
2018-11-13 23:04:56 +0800

    init

---
+++ src/main/java/com/huibudc/thymeLeafDemo/model/User.java
@@ -0,0 +1,43 @@
+package com.huibudc.thymeLeafDemo.model;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String address;
+
+    public User(int id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
